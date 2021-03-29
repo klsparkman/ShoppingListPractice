ba671f591e1e54194a63b11522c13f7047f4484f
Kelsey Sparkman <kelseysparkman14@gmail.com>
2021-03-29 14:09:58 -0600

    file structure and basic model setup

---
+++ ShoppingListPractice/Model/List.swift
@@ -0,0 +1,13 @@
+//
+//  List.swift
+//  ShoppingListPractice
+//
+//  Created by Kelsey Sparkman on 3/29/21.
+//
+
+import Foundation
+
+struct List {
+    let name: String
+    let isChecked: Bool
+}
